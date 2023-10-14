17ee5dc97b06f5753f5ca7026756829f3170ab6e
HaHaWTH <id_cn00@outlook.com>
2023-10-14 20:37:23 +0800

    Prepare for B27

--- src/main/java/fr/xephi/authme/AuthMe.java
+++ src/main/java/fr/xephi/authme/AuthMe.java
@@ -76,7 +76,7 @@ public class AuthMe extends JavaPlugin {
     // Version and build number values
     private static String pluginVersion = "5.6.0-Fork";
     private static final String pluginBuild = "b";
-    private static String pluginBuildNumber = "26";
+    private static String pluginBuildNumber = "27";
     protected final Boolean SHAEnabled = false;
     // Private instances
     private EmailService emailService;
