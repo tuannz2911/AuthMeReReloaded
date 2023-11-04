628d5adcb54baa737964e4cf09dff69540f3cabd
HaHaWTH <id_cn00@outlook.com>
2023-11-04 18:31:36 +0800

    MapUtils

---
+++ src/main/java/fr/xephi/authme/util/MapUtils.java
@@ -0,0 +1,23 @@
+package fr.xephi.authme.util;
+
+
+import org.bukkit.map.MapView;
+
+import java.lang.reflect.Method;
+
+public class MapUtils {
+    public static int getMapId(MapView mapView) {
+        try {
+            Method method = mapView.getClass().getMethod("getId");
+            Object mapId = method.invoke(mapView);
+            if (mapId instanceof Short) {
+                return ((Short) mapId).intValue();
+            } else {
+                return (Integer) mapId;
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return 0;
+    }
+}
