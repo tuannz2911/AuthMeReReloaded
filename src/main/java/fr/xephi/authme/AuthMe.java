23306d1f5c52dd68a6b09211bfb06cb5ec2bb5b0
HaHaWTH <id_cn00@outlook.com>
2023-11-25 19:15:19 +0800

    prepare for b34

--- src/main/java/fr/xephi/authme/AuthMe.java
+++ src/main/java/fr/xephi/authme/AuthMe.java
@@ -74,7 +74,7 @@ public class AuthMe extends JavaPlugin {
     // Version and build number values
     private static String pluginVersion = "5.6.0-Fork";
     private static final String pluginBuild = "b";
-    private static String pluginBuildNumber = "33";
+    private static String pluginBuildNumber = "34";
     // Private instances
     private EmailService emailService;
     private CommandHandler commandHandler;
