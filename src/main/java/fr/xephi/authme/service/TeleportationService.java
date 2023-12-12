a8ef8f3f3e60129ef1f63d2ba6c1b6f23485ffc2
HaHaWTH <id_cn00@outlook.com>
2023-12-13 01:19:19 +0800

    fix

--- src/main/java/fr/xephi/authme/service/TeleportationService.java
+++ src/main/java/fr/xephi/authme/service/TeleportationService.java
@@ -142,7 +142,8 @@ public class TeleportationService implements Reloadable {
             logger.debug("Teleporting `{0}` to spawn because of 'force-spawn after login'", player.getName());
             teleportToSpawn(player, true);
         } else if (settings.getProperty(TELEPORT_UNAUTHED_TO_SPAWN)) {
-            if (settings.getProperty(RestrictionSettings.SAVE_QUIT_LOCATION) && auth.getQuitLocY() != 0) {
+            if (settings.getProperty(RestrictionSettings.SAVE_QUIT_LOCATION) ||
+                (settings.getProperty(RestrictionSettings.SAVE_QUIT_LOCATION) && auth.getQuitLocY() != 0 && auth.getQuitLocY() == spawnLoader.getSpawn().getY())) {
                 Location location = buildLocationFromAuth(player, auth);
                 logger.debug("Teleporting `{0}` after login, based on the player auth", player.getName());
                 teleportBackFromSpawn(player, location);
