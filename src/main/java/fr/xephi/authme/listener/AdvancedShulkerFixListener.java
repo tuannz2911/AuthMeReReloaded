d8d3e8544ce3256a1513a7530a45053ac43d5b8d
HaHaWTH <id_cn00@outlook.com>
2023-12-31 21:25:42 +0800

    Remove useless bannerColor

--- src/main/java/fr/xephi/authme/listener/AdvancedShulkerFixListener.java
+++ src/main/java/fr/xephi/authme/listener/AdvancedShulkerFixListener.java
@@ -1,5 +1,6 @@
 package fr.xephi.authme.listener;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
@@ -13,31 +14,14 @@ public class AdvancedShulkerFixListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onDispenserActivate(BlockDispenseEvent event) {
         Block block = event.getBlock();
-        if (block.getFace(block) == BlockFace.DOWN) {
-            //If the block is in y = 0
-            if (block.getY() == 0) {
-                event.setCancelled(true);
-            }
+
+        if (block.getRelative(BlockFace.DOWN).getType().equals(Material.AIR) && block.getY() == 0) {
+            event.setCancelled(true);
         }
-        if (block.getFace(block) == BlockFace.UP) {
-            //If the block is in y = 255
-            if (block.getY() == 255) {
+        if (block.getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+            if (block.getY() == block.getWorld().getMaxHeight() - 1) {
                 event.setCancelled(true);
             }
         }
     }
-
-    //This implementation method will be available in my another plugin
-//    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-//    public void onDispenserBePlaced(BlockPlaceEvent event){
-//        Block block = event.getBlock();
-//        if (block.getType().equals(Material.DISPENSER)){
-//            if (block.getFace(block) == BlockFace.DOWN && block.getY() == 0){
-//                event.setCancelled(true);
-//            }
-//            if (block.getFace(block) == BlockFace.UP && block.getY() == 255){
-//                event.setCancelled(true);
-//            }
-//        }
-//    }
 }
