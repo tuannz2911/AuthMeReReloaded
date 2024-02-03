e3c460e614166d43a2b0f027ae552c9b86060648
HaHaWTH <id_cn00@outlook.com>
2024-02-03 20:07:32 +0800

    B42 release

--- src/main/java/fr/xephi/authme/AuthMe.java
+++ src/main/java/fr/xephi/authme/AuthMe.java
@@ -75,7 +75,7 @@ public class AuthMe extends JavaPlugin {
     // Version and build number values
     private static String pluginVersion = "5.6.0-Fork";
     private static final String pluginBuild = "b";
-    private static String pluginBuildNumber = "41";
+    private static String pluginBuildNumber = "42";
     // Private instances
     private EmailService emailService;
     private CommandHandler commandHandler;
