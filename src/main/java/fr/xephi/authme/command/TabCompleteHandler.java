7dc331eb07110e535c31d32d8a3d91a6c38a3960
HaHaWTH <id_cn00@outlook.com>
2024-03-25 12:24:53 +0800

    Fix tabcomplete

--- src/main/java/fr/xephi/authme/command/TabCompleteHandler.java
+++ src/main/java/fr/xephi/authme/command/TabCompleteHandler.java
@@ -5,13 +5,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TabCompleteHandler implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
-        return Collections.emptyList();
+        return new ArrayList<>();
     }
 }
