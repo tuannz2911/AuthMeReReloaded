9b74cb5a0048743e2620f62f1f01e9267d3b485a
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2024-06-12 01:45:57 +0800

    Fix Folia join commands

--- src/main/java/fr/xephi/authme/settings/commandconfig/CommandManager.java
+++ src/main/java/fr/xephi/authme/settings/commandconfig/CommandManager.java
@@ -129,9 +129,9 @@ public class CommandManager implements Reloadable {
             if (predicate.test(cmd)) {
                 long delay = cmd.getDelay();
                 if (delay > 0) {
-                    bukkitService.scheduleSyncDelayedTask(() -> dispatchCommand(player, cmd), delay);
+                    bukkitService.runTaskLater(player, () -> dispatchCommand(player, cmd), delay);
                 } else {
-                    dispatchCommand(player, cmd);
+                    bukkitService.runTaskIfFolia(player, () -> dispatchCommand(player, cmd));
                 }
             }
         }
